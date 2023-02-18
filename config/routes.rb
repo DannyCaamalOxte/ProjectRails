c1be4e2116e8bdd3a2ac15360ab59a41021ab6d1
DannyHasiel <90662918+DannyCaamalOxte@users.noreply.github.com>
2023-02-18 11:30:41 -0600

    Realizando modelos, y associations de active record

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :rooms
+  #ruta de inicio dirije al cotrolador pagina y accion home
+  root to: "pages#home"
+
+  get 'pages/home'
+  get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
