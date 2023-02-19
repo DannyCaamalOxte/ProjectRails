3068b0b7c2d1f15056d5697af0b1b4bbfd874998
DannyHasiel <90662918+DannyCaamalOxte@users.noreply.github.com>
2023-02-18 18:24:25 -0600

    Vista: Agregando navbar
    se agrego bootstrap y un navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   #ruta de inicio dirije al cotrolador pagina y accion home
   root to: "pages#home"
 
-  get 'pages/home'
-  get 'pages/about'
+  get "home", to: 'pages#home'
+  get "about", to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
