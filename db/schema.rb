f07365c62dc92f012a5dc6439c3ab7bbe1341783
DannyHasiel <90662918+DannyCaamalOxte@users.noreply.github.com>
2023-02-18 20:36:56 -0600

    Acción "index" en módulo Room
    index creado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_212050) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_19_004435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_212050) do
     t.bigint "hotel_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["hotel_id"], name: "index_rooms_on_hotel_id"
   end
 
