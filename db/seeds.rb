29305c05037f4869c31e34bee99f1f02a49b5b8d
DannyHasiel <90662918+DannyCaamalOxte@users.noreply.github.com>
2023-02-19 16:18:01 -0600

    listo

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,3 @@ Hotel.create!(
 )
 
 puts "Hotel creado"
-
-25.times do
-    Room.create(
-        length: rand(10),
-        width: rand(10),
-        taken: rand(2),
-        image: "https://via.placeholder.com/700x400",
-        description: Faker::Lorem.sentence(word_count: 100),
-        hotel_id: Hotel.first.id
-    )
-end
-
-puts "Habitaciones creadas"
